dbb8fc13807030762b94e22787ced28e6a3d85ab
viper_work <wiperawa@gmail.com>
2021-03-03 16:34:29 +0500

    currency-exchange-order crud fixes.

--- views/currency-exchange-order/view.php
+++ views/currency-exchange-order/view.php
@@ -26,8 +26,7 @@ $this->params['breadcrumbs'][] = '#' . $model->id;
                                            href="#" role="button"
                                            id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
                                            aria-expanded="false">
-                                            <?= $model->getAttributeLabel('Status') ?>
-                                            :&nbsp;<?= $model->isActive() ? 'active' : 'inactive' ?>
+                                            <?= $model->isActive() ? 'active' : 'inactive' ?>
                                         </a>
                                         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                                             <h6 class="dropdown-header"><?= $model->getAttributeLabel('Status') ?></h6>
